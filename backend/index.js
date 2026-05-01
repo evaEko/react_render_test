3cd947201560cdbdb2e7a25c37580e46c0e5d501
Eko <7765339+evaEko@users.noreply.github.com>
2026-05-01 16:37:04 +0200

    add fileURLToPath import and define __filename and __dirname

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 
+
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3001;
 const buildPath = path.join(__dirname, '../frontend/build');
